b4c128b1c88139214a412ecb30aa3def73858cf6
RISH-VERSION <harishff007113@gmail.com>
2025-07-04 09:49:09 +0530

    Initial commit: Spring Boot setup

---
+++ railway-reservation-api/src/main/java/com/brodev/railway_reservation_api/RailwayReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.brodev.railway_reservation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayReservationApiApplication.class, args);
+	}
+
+}
---
+++ railway-reservation-api/src/test/java/com/brodev/railway_reservation_api/RailwayReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brodev.railway_reservation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
